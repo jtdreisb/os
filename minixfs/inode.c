a744c4d13c86d4c0c5aaa71e8c520234d409d6c3
Jason Dreisbach <jtdreisb@gmail.com>
2011-06-03 19:13:04 -0700

    there is a bug with the big blocks

--- minixfs/inode.c
+++ minixfs/inode.c
@@ -13,7 +13,6 @@ void printInode(Inode *node) {
 }
 Inode * getInode(uint32_t num) {
     Inode *node;
-    int tbytes;
     int rbytes;
 
     num--;
@@ -22,6 +21,7 @@ Inode * getInode(uint32_t num) {
                     (sb->zsize*(2+sb->i_blocks+sb->z_blocks));
     /*printf("offset = %d\n", offset);*/
     /* seek to the inode in the bitmap */
+
     if(lseek(fd,globalOffset+offset,SEEK_SET) == -1) {
         return NULL;
     }
@@ -30,18 +30,23 @@ Inode * getInode(uint32_t num) {
     if (node == NULL) {
         return NULL;
     }
-    for (tbytes = 0; tbytes < sizeof(Inode);) {
+    /*for (tbytes = 0; tbytes < sizeof(Inode);) {*/
+        
         rbytes = read(fd, node, sizeof(Inode));
-        if (rbytes == -1) {
-            return NULL;
+        if (rbytes != sizeof(Inode)) {
+            fprintf(stderr, "inode num %u\n",num);
+            fprintf(stderr, "seeked to %u\n",offset);
+            fprintf(stderr, "READ failed: %d\n",rbytes);
+            exit(1);   
         }
+        /*if (rbytes == -1) {*/
+
+        /*}
         tbytes += rbytes;
-    }
+    }*/
 
     return node;
 }
-
-
 
 
 Dirent * findInode( Inode *node, 
